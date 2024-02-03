27bd898115b0d5a3954238a643ff6ea502b40280
idelovski <idelovski@yahoo.com>
2024-02-03 17:11:19 +0100

    ignore

--- tmp.m
+++ tmp.m
@@ -4,7 +4,6 @@ rotationTransform = CATransform3DScale (scalingTransform, 1.f, 0.8f, 1.f);
 
 self.arrowView.layer.transform = scalingTransform;
 
-
 gGPrefsRec.pfShowNumbers
 
 CGAffineTransform transform = CGAffineTransformMakeScale (1., 1.);
